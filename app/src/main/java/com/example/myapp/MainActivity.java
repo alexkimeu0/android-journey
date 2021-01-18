f1169e51b45f38edfdffbe153fdfa23461a64db5
alexkimeu0 <kim3ual3x@gmail.com>
2021-01-18 17:24:02 +0300

    Click events

---
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button)findViewById(R.id.btn);
+        TextView txt = (TextView)findViewById(R.id.txt);
+
+        // Set click listener on the button
+        btn.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        txt.setText("Who let the Dogs out?!");
+                    }
+                }
+        );
+
+        // Set on Long Click listener
+        btn.setOnLongClickListener(
+                new Button.OnLongClickListener(){
+                    public boolean onLongClick(View v){
+                        txt.setText("Am long pressed!!");
+                        return true;
+                    }
+                }
+        );
+    }
+
+
+}
