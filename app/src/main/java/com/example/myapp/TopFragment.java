e0918b9ecbaadee9b1037e8440b94b9c78a11f50
alexkimeu0 <kim3ual3x@gmail.com>
2021-01-21 22:03:15 +0300

    Implemented 2 fragments layouts

---
+++ app/src/main/java/com/example/myapp/TopFragment.java
@@ -0,0 +1,20 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TopFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.top_fragment, container, false);
+        return view;
+    }
+}
